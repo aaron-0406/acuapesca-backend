448c8a7f43fcd95668d1e4c3496897163d4f8fbd
Victor Hernandez <victor-2027@hotmail.com>
2022-05-29 22:19:38 -0500

    docs: :memo: ClsBDConexion documented

--- src/class/ClsBDConexion.ts
+++ src/class/ClsBDConexion.ts
@@ -1,12 +1,23 @@
-import { Pool } from "mysql2/promise";
 import { connect } from "../database";
 
+/*
+  Description: This class is for DataBase Connection
+*/
 class ClsBDConexion {
+  // for querys
   public conn: any;
+
+  /*
+    Description: The porpuse of this method is for to connect to DataBase
+  */
   async connectBD() {
     this.conn = await connect();
     console.log("Database connected");
   }
+
+  /*
+    Description: The porpuse of this method is to end the connection to DataBase
+  */
   async endConnection() {
     if (this.conn) await this.conn.end();
   }
